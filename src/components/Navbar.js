8f6943ce6e0a50f32a4f84bddda8b40e6e0c6fe8
khanalAjit <khanal.ajeeth32@gmail.com>
2022-12-14 17:52:35 +0545

    First Commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+//import { useState } from "react";
+function Navbar() {
+  //const [click, setClick] = useState();
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          {/*           <Link to="/" className="navbar-logo">
+            TRVL <i className="fab fa-typo3"></i>
+          </Link> */}
+          <h2>Hello</h2>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
